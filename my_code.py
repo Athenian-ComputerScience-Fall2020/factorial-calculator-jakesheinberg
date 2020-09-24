d152b0a19d41b75d39de60ba623c622ccb462ae4
jakesheinberg <23jsheinberg@athenian.org>
2020-09-24 04:31:50 +0000

    actually passed and done

--- my_code.py
+++ my_code.py
@@ -1,22 +1,22 @@
 
 
 def factorial_calc(x):   #you may choose the name of the parameter
-    x=1
+    y=1
     total =1
-    if x==0:
+    if y==0:
         return 1
     else:
-        while x<num+1:
-            total=total*x
-            x=x+1
+        while y<x+1:
+            total=total*y
+            y=y+1
         return total    # be sure to return the factorial
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-     #print(factorial_calc(1))
+    print(factorial_calc(5))
 
     #After you are satisfied with your results, use input() to prompt the user for a value:
-    num = int(input("Enter value to factorialize: "))
-    print(factorial_calc(int(num)))
+    #num = int(input("Enter value to factorialize: "))
+    #print(factorial_calc(int(num)))
