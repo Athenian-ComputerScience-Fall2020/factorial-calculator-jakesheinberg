271debb0ac8828ff3d7553ea70303bc3703d20b7
jakesheinberg <23jsheinberg@athenian.org>
2020-09-24 04:28:47 +0000

    done

--- my_code.py
+++ my_code.py
@@ -1,23 +1,22 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  none
-##i messed up the if name == main part
-##also need to add something for 0!
+
 
 def factorial_calc(x):   #you may choose the name of the parameter
-    pickanumber=int(input("pick a number:  "))
     x=1
     total =1
-    while x<pickanumber+1:
-        total=total*x
-        x=x+1
-    return total    # be sure to return the factorial
+    if x==0:
+        return 1
+    else:
+        while x<num+1:
+            total=total*x
+            x=x+1
+        return total    # be sure to return the factorial
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-     print(factorial_calc)
+     #print(factorial_calc(1))
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #num = input("Enter value to factorialize: ")
-    #print(factorial_calc(int(num)))
+    #After you are satisfied with your results, use input() to prompt the user for a value:
+    num = int(input("Enter value to factorialize: "))
+    print(factorial_calc(int(num)))
